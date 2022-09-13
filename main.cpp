302e3838293b7a7147628b1ce4a7fb5c1c9b4701
Max Hamme <81451434+Al9haPh0enix@users.noreply.github.com>
2022-09-12 21:28:21 -0700

    fix a couple bugs

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 
 int main() {
-    std::cout << "I have full access to everything.\n\n-Mx\n\n";
+    std::cout << "I have full access to everything.\n\n-Max\n"<<std::endl;
+
+    while(true){}
+
     return 0;
 }
